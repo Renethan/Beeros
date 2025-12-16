195a48818b9dc3f02001874957c77b26ea9eaa1a
Renethan <ethan.rene83560@gmail.com>
2025-12-16 13:49:04 +0100

    update architecture 2

--- scripts/cssChanger.js
+++ scripts/cssChanger.js
@@ -3,13 +3,13 @@ function changerCSS(){
     var O_baliseMetaCss = document.getElementById("css");
     var S_css = O_baliseMetaCss.getAttribute("href");
 
-    if(S_css == "../css/style.css"){
+    if(S_css == "css/style.css"){
         console.log("Le CSS utilisé est :" + S_css);
-        O_baliseMetaCss.setAttribute("href","../css/style2.css");
+        O_baliseMetaCss.setAttribute("href","css/style2.css");
         console.log("Maintenant c'est: style2.css");
     }else{
         console.log("Le CSS utilisé est :" + S_css);
-        O_baliseMetaCss.setAttribute("href","../css/style.css");
+        O_baliseMetaCss.setAttribute("href","css/style.css");
         console.log("Maintenant c'est: style.css");
     }
 
